81de0457a2c9d409042dcb1485b3c56457fb9ee1
kartikjindgar <41141983+kartikjindgar@users.noreply.github.com>
2020-07-07 13:41:03 +0530

    Delete twitter_key.py

--- twitter_key.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'TVBmzGtSMrevajhqRRTcFApPt'
-consumer_secret = 'plz1cQOwLp5HX7yPo8FTIpnJo7XlCnHuCdsyY3JnLQF3w3Ugwa'
-access_token = '2476671038-IOfmxsGA18fSggHlD70uQqLPE7Ax9SLIU01tchn'
-access_token_secret = 'mhnZWyXcRSCKVZULtcKlYdMzL3tOwx31usQwaVa94rcrP'	
